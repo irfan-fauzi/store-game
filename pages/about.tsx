03e35f12ef351dfa21b05a74d41e963406a92fc6
irfanfauzi <irfanfauzi24@yahoo.com>
2021-10-04 22:57:50 +0700

    sip

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,29 +7,28 @@ interface AboutProps {
 }
 
 const About = (props: AboutProps) => {
-  const {dataUser} = props
-  const router = useRouter()
+  const { dataUser } = props
   return (
     <Layout titlePage="about">
-    <div>
-      <h1>About Page</h1>
-      {
-        dataUser.map(el => (
-          <ul key={el.id} >
-            <li className="hover" onClick={() => router.push(`/detail/${el.id}`)}>{el.title}</li>
-          </ul>
-        ))
-      }
-    </div>
+      <div>
+        <h1>About Page</h1>
+        {
+          dataUser.map(el => (
+            <ul key={el.id} >
+              <li className="hover">{el.title}</li>
+            </ul>
+          ))
+        }
+      </div>
     </Layout>
   )
 }
 
 export default About
 
-export const getStaticProps = async() => {
+export const getStaticProps = async () => {
   const url = "https://jsonplaceholder.typicode.com/posts"
   const result = await fetch(url)
   const dataUser = await result.json()
-  return { props: {dataUser} }
+  return { props: { dataUser } }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
-import type { NextPage } from 'next'
+
 import Layout from '../components/layout'
-import Image from 'next/image'
 
-const Home: NextPage = () => {
+
+interface AboutProps {
+  dataUser: Array<any>
+}
+
+const Home = (props: AboutProps) => {
+  const {dataUser} = props
+  
   return (
-
   <Layout titlePage="home">
    <div>
      <h1>Home Page</h1>
-     <Image src="/1.jpg" width={600} height={400} alt="profile"/>
-     <p>img normal : </p>
+        {
+          dataUser.map(el => (
+            <ul key={el.id} >
+              <li className="hover">{el.title}</li>
+            </ul>
+          ))
+        }
      
       <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi saepe aperiam voluptate quae sapiente ut? Beatae, aliquid atque porro voluptatem mollitia ad, quia ab eveniet distinctio nihil aut ex fugit doloribus? Iste molestias aliquam omnis veniam beatae aperiam est assumenda doloremque laudantium, illum deleniti quisquam amet! Error tempora repellat, aut enim cumque, non ullam quis quas qui quidem reprehenderit ab voluptatibus labore! Et, magni illo! Deleniti aliquam tempora necessitatibus nam natus modi suscipit atque ipsum. Aliquam temporibus in officiis laboriosam inventore totam ipsum iste vitae ducimus dolorem rem, dolor repellat doloribus eius alias veniam omnis! Saepe nemo ducimus adipisci! Autem?</h4>
       
@@ -19,3 +29,10 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getStaticProps = async () => {
+  const url = "https://jsonplaceholder.typicode.com/posts"
+  const result = await fetch(url)
+  const dataUser = await result.json()
+  return { props: { dataUser } }
+}
